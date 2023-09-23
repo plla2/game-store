2a58f264e065c0fb3116febaa673614ff65fca64
plla2 <jhbljs92@naver.com>
2023-09-23 23:55:07 +0900

    💄 Header 스타일

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import { IoGameController, IoCart } from "react-icons/io5";
 import SearchBar from "../SearchBar/SearchBar";
+import "../../styles/_header.scss";
 
 interface Props {
   cartItem: unknown[];
@@ -9,7 +10,7 @@ interface Props {
 
 const Header = ({ cartItem }: Props) => {
   return (
-    <header>
+    <header className="Header">
       <Link to="/">
         <Button className="Logo">
           <IoGameController />
@@ -19,6 +20,7 @@ const Header = ({ cartItem }: Props) => {
       <SearchBar />
       <Button className="Cart">
         <IoCart />
+        Cart
         <span>{cartItem.length}</span>
       </Button>
     </header>
