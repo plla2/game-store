42dec97dfe9ffd82c4715122a18d2fcc94badc6e
plla2 <jhbljs92@naver.com>
2023-09-24 00:42:34 +0900

    💄 Button 컴포넌트 , App전체 스타일

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
+import "./styles/App.scss";
 
 const App = () => {
   const [cartItem] = useState([]);
 
   return (
-    <>
+    <div className="App">
       <Header cartItem={cartItem} />
       <Routes>
         <Route path="/" element={null} />
       </Routes>
-    </>
+    </div>
   );
 };
 
