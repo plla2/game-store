30c4e58faeabe538fc96e069df4b2db6aa90d58a
plla2 <jhbljs92@naver.com>
2023-09-23 21:06:29 +0900

    ✨ Router 연결

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./fonts/fontStyleSheets.scss";
 import "./style.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
