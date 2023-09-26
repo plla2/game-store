169e184dbd7d4c6d2f30eca85d2d247e2926f345
plla2 <jhbljs92@naver.com>
2023-09-27 01:14:36 +0900

    ✨ Home, Loading 조건부 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import "./styles/App.scss";
+import { Temporal } from "temporal-polyfill";
+import { gameList } from "./apis/gameList";
+import Home from "./pages/Home/Home";
+import Loading from "./components/Loading/Loading";
 
 const App = () => {
   const [cartItem] = useState([]);
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const loadGames = async () => {
+      const today = Temporal.Now.plainDateISO();
+      const threeMonthAgo = today.subtract({ months: 3 });
+      const response = await gameList({
+        page_size: 50,
+        dates: `${threeMonthAgo},${today}`,
+      });
+      setGames(response.results as []);
+    };
+    loadGames();
+  }, []);
 
   return (
     <div className="App">
       <Header cartItem={cartItem} />
       <Routes>
-        <Route path="/" element={null} />
+        <Route
+          path="/"
+          element={games.length ? <Home games={games} /> : <Loading />}
+        />
       </Routes>
     </div>
   );
