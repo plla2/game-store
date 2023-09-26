f726a2608dd282eab028bbdfbc98dc788a622bf4
plla2 <jhbljs92@naver.com>
2023-09-27 01:11:03 +0900

    ✨ Home 컴포넌트 작성

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { Game } from "../../types/Game.types";
+import { motion } from "framer-motion";
+import GameCard from "../../components/GameCard/GameCard";
+
+interface Props {
+  games: Game[];
+}
+
+const cardDuration = 10;
+const cycleArray = (array: Game[]) => {
+  const newArr = [...array];
+  newArr.push(newArr.shift() as Game);
+  return newArr;
+};
+
+const Home = ({ games }: Props) => {
+  const [homeGames, setHomeGames] = useState(games.slice(0, 4));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHomeGames((games) => cycleArray(games));
+    }, cardDuration * 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  console.log(homeGames);
+  return (
+    <motion.div>
+      {homeGames.map((game, index) => (
+        <GameCard
+          key={game.id}
+          id={game.id}
+          name={game.name}
+          backgroundImage={game.background_image}
+          duration={cardDuration}
+          big={index === 0}
+        />
+      ))}
+    </motion.div>
+  );
+};
+
+export default Home;
