3419d817f82b51a75f8c12e00ad52d401de0fe85
plla2 <jhbljs92@naver.com>
2023-09-28 20:35:56 +0900

    ✨ motion.div 속성 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -26,7 +26,12 @@ const Home = ({ games }: Props) => {
 
   console.log(homeGames);
   return (
-    <motion.div>
+    <motion.div
+      className="Home"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
       {homeGames.map((game, index) => (
         <GameCard
           key={game.id}
