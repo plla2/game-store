e1ec28d7401c603e9682e2af8261eedbe597e6fd
plla2 <jhbljs92@naver.com>
2023-09-29 23:15:12 +0900

    ✨ Transition 컴포넌트 추가

---
+++ src/components/Transition/Transition.tsx
@@ -0,0 +1,56 @@
+import { motion } from "framer-motion";
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+  direction?: "left" | "right" | "up" | "down" | "none";
+  distance?: number;
+}
+
+const Transition = ({
+  children,
+  className,
+  direction = "none",
+  distance = 50,
+}: Props) => {
+  const directions = {
+    left: { x: -distance },
+    right: { x: distance },
+    up: { y: distance },
+    down: { y: -distance },
+    none: { x: 0, y: 0 },
+  };
+
+  const animateConfig = {
+    in: {
+      opacity: 0,
+      ...directions[direction],
+    },
+    animate: {
+      opacity: 1,
+      ...directions.none,
+      transition: {
+        x: { type: "spring" },
+        y: { type: "spring" },
+      },
+    },
+    out: {
+      opacity: 0,
+      ...directions[direction],
+      transition: { type: "just" },
+    },
+  };
+  return (
+    <motion.div
+      className={className}
+      variants={animateConfig}
+      initial="in"
+      animate="animate"
+      exit="out"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default Transition;
