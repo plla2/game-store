30203fb596147664c6ce0d2c165b6839dff2f2f4
plla2 <jhbljs92@naver.com>
2023-09-29 15:12:00 +0900

    🩹 Temporal 라이브러리 삭제 후 Date메서드 사용

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import "./styles/App.scss";
-import { Temporal } from "temporal-polyfill";
 import { gameList } from "./apis/gameList";
 import Home from "./pages/Home/Home";
 import Loading from "./components/Loading/Loading";
@@ -11,18 +10,22 @@ const App = () => {
   const [cartItem] = useState([]);
   const [games, setGames] = useState([]);
 
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = `0${1 + today.getMonth()}`.slice(-2);
+  const day = `0${today.getDate()}`.slice(-2);
+  const date = `${year}-${month}-${day}`;
+
   useEffect(() => {
     const loadGames = async () => {
-      const today = Temporal.Now.plainDateISO();
-      const threeMonthAgo = today.subtract({ months: 3 });
       const response = await gameList({
         page_size: 50,
-        dates: `${threeMonthAgo},${today}`,
+        dates: date,
       });
       setGames(response.results as []);
     };
     loadGames();
-  }, []);
+  }, [date]);
 
   return (
     <div className="App">
