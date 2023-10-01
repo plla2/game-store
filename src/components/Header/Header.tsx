3dee11073581d126a263d8ede08998f0dd2b138f
plla2 <jhbljs92@naver.com>
2023-10-02 00:45:14 +0900

    ♻️ Header 컴포넌트 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,16 @@ import Button from "../Button/Button";
 import { IoGameController, IoCart } from "react-icons/io5";
 import SearchBar from "../SearchBar/SearchBar";
 import "../../styles/_header.scss";
+import Transition from "../Transition/Transition";
+import { Game } from "../../types/Game.types";
 
 interface Props {
-  cartItem: unknown[];
+  cartItem: Game[];
 }
 
 const Header = ({ cartItem }: Props) => {
   return (
-    <header className="Header">
+    <Transition className="Header" direction="down" distance={20}>
       <Link to="/">
         <Button className="Logo">
           <IoGameController />
@@ -23,7 +25,7 @@ const Header = ({ cartItem }: Props) => {
         Cart
         <span>{cartItem.length}</span>
       </Button>
-    </header>
+    </Transition>
   );
 };
 
