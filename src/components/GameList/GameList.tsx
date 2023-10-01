8b76e40fc6ad1234e36efc29fe37ff2e1f867a29
plla2 <jhbljs92@naver.com>
2023-10-02 00:12:14 +0900

    ✨ search param 기능

--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -1,49 +1,55 @@
-import { LayoutGroup } from "framer-motion";
+import { motion } from "framer-motion";
 import { Game } from "../../types/Game.types";
 import Transition from "../Transition/Transition";
-import GameListCard from "../GameListCard/GameListCard";
-import { useWindowWidth } from "@react-hook/window-size";
 import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import Button from "../Button/Button";
+import { RiArrowLeftLine } from "react-icons/ri";
+import Loading from "../Loading/Loading";
+import Grid from "../Grid/Grid";
 
 interface Props {
   games: Game[];
+  loadGames: (search: string) => Promise<Game[]>;
 }
 
-const GameList = ({ games }: Props) => {
-  const windowWidth = useWindowWidth();
-  const [columns, setColumns] = useState(0);
-  const gamesPerColumns = Math.ceil(games.length / columns);
+const GameList = ({ games, loadGames }: Props) => {
+  const [displayGames, setDisplayGames] = useState(games);
+  const [searchParams] = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setColumns(Math.floor(windowWidth / 330) || 1);
-  }, [windowWidth]);
+    const searchParam = searchParams.get("search") || "";
+    console.log(searchParam);
+    setIsLoading(true);
+    if (searchParam) {
+      (async () => {
+        setDisplayGames(await loadGames(searchParam));
+        setIsLoading(false);
+      })();
+    } else if (games.length) {
+      setDisplayGames(games);
+      setIsLoading(false);
+    }
+  }, [games, loadGames, searchParams]);
 
   return (
     <Transition className="GameList" direction="right">
-      <h2>Best of All Time</h2>
-      <div className="Grid">
-        <LayoutGroup>
-          {Array(columns)
-            .fill(null)
-            .map((_, index) => {
-              const gamesToDisplay = [];
-              for (let i = 0; i < gamesPerColumns; i++) {
-                const gameIndex = i * columns + index;
-                if (gameIndex < games.length) {
-                  gamesToDisplay.push(games[gameIndex]);
-                }
-                console.log(gamesToDisplay);
-              }
-              return (
-                <div key={`column-${index}`} className="Column">
-                  {gamesToDisplay.map((game) => (
-                    <GameListCard key={game.id} {...game} />
-                  ))}
-                </div>
-              );
-            })}
-        </LayoutGroup>
-      </div>
+      <nav>
+        {searchParams.get("search") && (
+          <Transition direction="left">
+            <Link to="/games">
+              <Button className="Store">
+                <RiArrowLeftLine /> Store
+              </Button>
+            </Link>
+          </Transition>
+        )}
+        <motion.h2 layout>
+          {searchParams.get("search") || "Best of All Time"}
+        </motion.h2>
+      </nav>
+      {isLoading ? <Loading /> : <Grid games={displayGames} />}
     </Transition>
   );
 };
