fc4cbabac3be56608a41be9ae6e2c0b8e4da582d
plla2 <jhbljs92@naver.com>
2023-10-03 14:25:32 +0900

    🔥 StrictMode 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./fonts/fontStyleSheets.scss";
@@ -6,11 +5,9 @@ import "./style.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
