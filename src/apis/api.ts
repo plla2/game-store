a83dd201110de708c8fd395352a08a0e438b3c0e
plla2 <jhbljs92@naver.com>
2023-10-08 23:56:56 +0900

    ⚡️ data localstorage등록

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -5,6 +5,10 @@ const api = {
   key: "1d2b5d761b684c5da2d5e4ee96fd0092",
 };
 
+const cachedRequests = JSON.parse(
+  localStorage.getItem("cachedRequests") || "{}"
+);
+
 interface ResponseSchema<T> {
   count: number;
   next: string;
@@ -16,11 +20,15 @@ async function get<T>(
   endPoint: string,
   params?: Record<string, string>
 ): Promise<T> {
-  const searchParams = new URLSearchParams(params);
+  const searchParams = new URLSearchParams(params).toString();
+  if (cachedRequests[searchParams])
+    return cachedRequests[searchParams] as Promise<T>;
   const res = await axios.get(
     `${api.url}${endPoint}?${searchParams}&key=${api.key}`
   );
   if (!res.data) throw new Error(res.statusText);
+  cachedRequests[searchParams] = res.data;
+  localStorage.setItem("cachedRequests", JSON.stringify(cachedRequests));
   return res.data;
 }
 
