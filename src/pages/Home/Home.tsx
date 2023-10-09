2170a5ec0617ad87dc9a170886b114db99394426
plla2 <jhbljs92@naver.com>
2023-10-09 18:02:45 +0900

    🩹 에러 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -45,6 +45,7 @@ const Home = ({ setGames, loadGames }: Props) => {
       }, cardDuration * 1000);
     })();
     return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
