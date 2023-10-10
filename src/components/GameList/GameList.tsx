6f5f0145b3627c6c697d4e392d566e62b00e3fb8
plla2 <jhbljs92@naver.com>
2023-10-10 14:25:42 +0900

    🚑 무한 리렌더링 문제 해결

--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Game } from "../../types/Game.types";
 import Transition from "../Transition/Transition";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useSearchParams } from "react-router-dom";
 import Loading from "../Loading/Loading";
 import Grid from "../Grid/Grid";
 import Navbar from "../Navbar/Navbar";
+import { useAnimation } from "framer-motion";
 
 interface Props {
   games: Game[] | null;
@@ -24,18 +25,28 @@ const GameList = ({
 }: Props) => {
   const [searchParams] = useSearchParams();
   const location = useLocation();
+  const [scrollY, setScrollY] = useState(0);
+  const controls = useAnimation();
 
   useEffect(() => {
     if (location.pathname === "/games") {
       setGames(null);
+      const scrollToTop = async () => {
+        await controls.start({ opacity: 0 });
+        window.scrollTo(0, 0);
+        setScrollY(0);
+        await controls.start({ opacity: 1 });
+      };
       if (location.search) {
+        scrollToTop();
         (async () =>
           setGames(await loadGames(searchParams.get("search") || "")))();
       } else {
+        window.scrollTo(0, scrollY);
         (async () => setGames(await loadGames()))();
       }
     }
-  }, [searchParams, location]);
+  }, [searchParams]);
 
   return (
     <Transition className="GameList" direction="right">
