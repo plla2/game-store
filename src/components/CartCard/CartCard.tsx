d5eed66b118950b5c5ad2a280fa93f4a1e44f591
plla2 <jhbljs92@naver.com>
2023-10-14 16:36:59 +0900

    ✨ Cart에서 제목클릭시 details페이지로 이동

--- src/components/CartCard/CartCard.tsx
+++ src/components/CartCard/CartCard.tsx
@@ -2,15 +2,20 @@ import { AiOutlineClose } from "react-icons/ai";
 import { Game } from "../../types/Game.types";
 import Button from "../Button/Button";
 import Transition from "../Transition/Transition";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   cart: Game;
   removeCartItem: (id: number) => void;
 }
 const CartCard = ({ cart, removeCartItem }: Props) => {
+  const navigate = useNavigate();
+  const goGame = () => {
+    navigate(`/games/${cart.id}`);
+  };
   return (
     <Transition className="Item" layout direction="right" durationOut={0.15}>
-      <Button>{cart.name}</Button>
+      <Button handleClick={() => goGame()}>{cart.name}</Button>
       <p>{`$ ${cart.price}`}</p>
       <Button
         className="Remove"
